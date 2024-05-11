00d34a5e3ff65f485615fbd056c4dc708c997909
schemalem <nschembri93@gmail.com>
2024-05-11 16:22:06 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `All Things Alpaca UK`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
